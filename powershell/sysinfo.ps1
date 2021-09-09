aa0226dc671cb37e85a8fd82d292cd254109c813
uc-mestemax <mestemax@mail.uc.edu>
2021-09-09 15:35:33 -0400

    powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,8 @@
+﻿function getIP {
+    (Get-NetIPAddress).IPv4Address | Select-String "10.*"
+}
+
+$IP = getIP
+Write-Host("This machine's IP is $IP")
+Write-Host("This machine's IP is {0}" -f $IP)
+
