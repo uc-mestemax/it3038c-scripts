014c920486e645c8a97955038b93a3521443b734
uc-mestemax <mestemax@mail.uc.edu>
2021-10-10 21:08:56 -0400

    Lab 6

--- node/server.js
+++ node/server.js
@@ -1,49 +1,56 @@
-var fs = require("fs")
+var fs = require("fs");
 var http = require('http');
-var ip = require ('ip');
-var os = require('os')
+var ip = require('ip');
+var os = require('os');
 
-function getUptime() {
-    serverTime = os.Uptime();
-    //days = serverTime.math.floor
-    //https://github.com/uc-botheaj/it3038c-scripts/tree/main/node
-    return serverTime
+function getuptime() {
+    serverTime = os.uptime();
+    var serverMinutes = Math.floor(serverTime/60)
+    var ServerHours = Math.floor(serverMinutes/60)
+    var ServerDays  = Math.floor(ServerHours/24)
 
+    var concatenatedTime = "Days: " + ServerDays + ", Hours: " + ServerHours%24 + ", Minutes: " + serverMinutes%60 + ", Seconds: " + serverTime%60
+    return concatenatedTime;
 }
-var server = http.createServer(function(req, res){
+
+//days = serverTime.math.floor
+    //https://github.com/uc-botheaj/it3038c-scripts/tree/main/node
+
+var server = http.createServer(function (req, res) {
     if (req.url === "/") {
-        
-        fs.readFile("./public/index.html", "UTF-8", function(err, body) {
-            res.writeHead(200, {"Content-Type":"text/html"});
+
+        fs.readFile("./public/index.html", "UTF-8", function (err, body) {
+            res.writeHead(200, { "Content-Type": "text/html" });
             res.end(body);
         });
     }
     else if (req.url.match("/sysinfo")) {
-        myHostName=os.hostname();
+        myHostName = os.hostname();
 
-        html =  `<!DOCTYPE HTML>
+        html = `
+        <!DOCTYPE HTML>
                 <html>
-                    <head> <title>Node JS Response</title> </head>
+                    <head> 
+                        <title>Node JS Response</title> 
+                    </head>
                     <body>
                         <p>Hostname: ${myHostName}</p>
-                        <p>IP: ${ip.address}</p>
-                        <p>Server Uptime: ${getUptime}</p>
-                        <p>Total Memory: </p>
-                        <p>Free Memory: </p>
-                        <p>Number of CPUs: </p>
+                        <p>IP: ${ip.address()}</p>
+                        <p>Server Uptime: ${getuptime()}</p>
+                        <p>Total Memory: ${(os.totalmem()/1024/1024)} MB</p>
+                        <p>Free Memory: ${os.freemem()/1024/1024} MB</p>
+                        <p>Number of CPUs: ${os.cpus().length} CPUs</p>
 
                     </body>
                 </html>`
-                res.writeHead(200, {"Content-Type":"text.html"});
-                res.end(html);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(html);
     }
     else {
-        res.writeHead(404, {"Content-Type": "text/html"})
-        res.end('404 file not found at ${req.url}');
+        res.writeHead(404, { "Content-Type": "text/html" })
+        res.end(`404 file not found at ${req.url}`);
     }
 
-});
-
-server.listen(3000)
+}).listen(3000)
 
 console.log("Server listening on port 3000");
