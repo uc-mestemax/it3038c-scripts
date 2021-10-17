48ac357ab482e07862253ed6358a97fff4bc4ee9
uc-mestemax <mestemax@mail.uc.edu>
2021-10-17 19:17:09 -0400

    Lab 7

---
+++ labs/Lab 7/Lab 7.ps1
@@ -0,0 +1,20 @@
+﻿$myIP = (Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias Ethernet).IPAddress
+$mySubnetMask = (Get-NetIPAddress -AddressFamily IPV4 -InterfaceAlias Ethernet).PrefixLength
+#Write-Host "Your IP is " $myIP
+
+#Write-Host '********Subnet Statistics********'
+#New-IPCalcNetwork -NetworkID $myIP -NetworkMaskLength $mySubnetMask
+
+$newSubnet = New-IPCalcNetwork -NetworkID $myIP -NetworkMaskLength $mySubnetMask
+
+Write-Host '*********Dividing the Subnet*********'
+#$newSubnet.Split(4)
+
+#Write-Host $newSubnet
+
+
+$newSubnet.ContainsIP('10.217.16.1')
+
+
+
+New-IPCalcNetwork -NetworkID $myIP -NetworkMaskLength $mySubnetMask
