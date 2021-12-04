18751aa7abc2ce3b63d8956ff0e3ebfcdae000e0
uc-mestemax <mestemax@mail.uc.edu>
2021-12-04 13:11:10 -0500

    project3

--- project3/readme.md
+++ project3/readme.md
@@ -1,3 +1,8 @@
 This project really told me what I didn't know and the breadth of many languages, so I am hoping to strengthen those skills as I move forward. I also did this with flask, and oh my gosh, it was so much more intuitive for me. Even though I spent the most time on the nodeJS portion for project three, NodeJS continues to keep me awake at night and give me nightmares, but nonetheless I learned a lot.
 
 The big caveat or downfall of using heroku is that each time a form gets submitted, it overwrites the global variable of the form input that gets sent, which will change the parameters that the scraper depends on to fetch the ETA. So, essentially only one person can use it at a time, but I just wanted to throw it on heroku for the laughs. Probably not the best idea to deploy a webscraper there either I'd imagine.
+
+Nonetheless, it was incredibly rewarding to accomplish this scraper in powershell, nodejs, and python (still gotta try it in bash). Deploying it in heroku was another improvement that allows me to use the scraper on my phone. This was a lot of fun!
+
+
+Every time the ETA is refreshed you will notice it changes color. 
--- project3/server.js
+++ project3/server.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var fs = require('fs').promises;
+var fs = require('fs');
 var express = require("express"); //This library is used to collect the form data from index.html
 var ip = require('ip');
 var os = require('os');
@@ -42,80 +42,108 @@ var server = http.createServer(async function (req, res) {
     `<!DOCTYPE HTML>
     <html lang="en">
         <head> 
-            <title>Node JS Response</title> 
+            <title>UC Shuttle Scraper</title> 
             <meta charset="UTF-8">
+            <link rel="icon" type="image/x-icon" href="favicon.ico">
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
             <!-- Optional theme -->
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
         </head>
-        <body>
-                <h3 id="input" style="text-align: center;">Your Stop ID:</h3>
-                <p style="text-align: center;">The current ETA is <span id="ETA"></span></p>
+        <body style="margin: 50px">
+        <h1 style="color: red; text-align: center;">UC Shuttle Scraper</h1>
+                <p style="text-align: center; font-size:large;">The current ETA at the designated stop location is <b><span id="ETA">(awaiting for user)</span></b> minute(s) until the shuttle arrives</p>
             
-            <form onsubmit="document.getElementById('form').style.display = 'none' id="form" style="text-align: center; margin-left: 30vw; margin-right: 30vw;" method="post" action="http://localhost:3000/post-eta">
+            <form id="form" style="text-align: center; margin-left: 30vw; margin-right: 30vw;" method="post" action="http://localhost:3000/post-eta">
                 <div class="form-group">
                     <label for="stopID">What is your shuttle stop number?</label>
                     <input type="number" min="0" required="required" name="stopID" placeholder="Shuttle Stop ID"" class="form-control" >
                     <small id="stopIDHelp" class="form-text text-muted">This is found in the of your doublemap json URL: eta?stop=??</small>
                 </div>
                 <div class="form-group">
-                    <label for="objectPosition">What is your object position?</label>
+                    <label for="objectPosition">What is your ETA position</label>
                     <input type="number" min="0" required="required" name="objectPosition" class="form-control" placeholder="Object Position">
+                    <small id="stopIDHelp" class="form-text text-muted">This is found in the json data right above the avg: key. 0 is for the first bus.</small>
+                    <br> <br>
                     <input type="submit" class="btn btn-primary" value="Submit">
                 </div>
+                <input type="button" class="btn btn-primary" id="button_A" style="text-align=center;" value="Click Me to Set a Reminder" onclick="let timeToAlert = prompt('How many minutes until arrival would you like to be notified?');">
             </form>
 
-            <input type="button" value="Click Me to Set a Reminder" onclick="var timeToAlert = prompt('How many minutes until arrival would you like to be notified?')">
 
         </body>
         <script>
-            window.onload = function(){
-            //var timeToAlert = prompt("How many minutes until arrival would you like to be notified?");
-            window.setInterval( async function() {
-                //Because the shuttle doesn't decremement by 1 each time it updates, some of the IF statements will completely be missed because for example, 
-                //it can jump from 6 to 4 without showing 5. These also only allow the if statement to run once per browser session.
-                let has5MinutesBeenUsed = 0
-                let has1MinuteBeenUsed = 0
-                let has0MinutesBeenUsed = 0
-                let hastimeToAlertBeenUsed = 0
+        
 
+
+
+            window.onload = function(){
+
+                //Because the shuttle doesn't decremement by 1 each time it updates, some of the IF statements will completely be missed because for example, 
+                //it can jump from 6 to 4 without showing 5. These variables below will only allow the if statement to run once per browser session, and ensure each if statement 
+                //gets run at some point
+
+            const buttonA = document.querySelector('#button_A');
+            let timeToAlert = -1;
+
+            buttonA.onclick = function() {
+                
+                timeToAlert = prompt('How many minutes until arrival would you like to be notified?');
+                while (!/^[0-9]+$/.test(timeToAlert)) {
+                    alert("You did not enter a number.");
+                    timeToAlert = prompt('How many minutes until arrival would you like to be notified?');
+                }
+                console.log('Time to alert has been set to ' + timeToAlert)
+              }
+            let has5MinutesBeenUsed = 0;
+            let has1MinuteBeenUsed = 0;
+            let has0MinutesBeenUsed = 0;
+            let hastimeToAlertBeenUsed = 0;
+
+            function randomColor() { // I wanted to make it flash every time the ETA value is different from the previous one, but that is outside my javascript grasp right now. Come back to this later, I already invested too much time.
+                document.getElementById('ETA').style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+              }
+            
+            let etaCalculation = window.setInterval( async function() {
                 let response = await fetch('http://localhost:3000/get-eta');
+                randomColor()
                 message = await response.text()
                 document.getElementById('ETA').innerHTML = message
                 isItTimeToLeave(message)
-                /*function isItTimeToLeave(eta) {
+                function isItTimeToLeave(eta) { 
+                    eta = parseInt(eta) //converts ETA to an INT
+                    timeToAlert = parseInt(timeToAlert)
                     console.log('The ETA for the shuttle is ' + eta + ' minute(s)')
+
                     //These are a series of if statements that are checking the ETA compared to the time you need to leave.
-                    if (eta <= '5' && has5MinutesBeenUsed = 0) {
-                        console.log('There is five minutes until the shuttle is arriving!')
-                        has5MinutesBeenUsed = 1
-                        alert('There is 5 minutes!')
+                    if (eta <= timeToAlert && hastimeToAlertBeenUsed === 0) {
+                        console.log('Heads up! Your designated time of ' + timeToAlert + ' has come!')
+                        hastimeToAlertBeenUsed = 1
+                        alert('Time to leave! Your designated time has arrived at ' + eta + ' minute(s)')
                     }
-                    else if (eta <= '1' && has1MinuteBeenUsed = 0) {
-                        console.log('There is one minute until the shuttle is arriving!')
+
+                    else if (eta <= 5 && has5MinutesBeenUsed === 0) {
+                        console.log('There is five minutes or less until the shuttle is arriving!')
+                        has5MinutesBeenUsed = 1
+                        alert('There is five minutes until the shuttle is arriving!')
+                    }
+
+                    else if (eta <= 1 && has1MinuteBeenUsed === 0) {
+                        console.log('There is one minute or less until the shuttle is arriving!')
                         has1MinuteBeenUsed = 1
-                        alert('There is 1 minute!')
+                        alert('There is 1 minute until the shuttle arrives!')
                 
                     }
                   
-                    else if (eta <= '0' && has0MinutesBeenUsed = 0) {
+                    else if (eta <= 0 && has0MinutesBeenUsed === 0) {
                         console.log('The shuttle is arriving!')
                         has0MinutesBeenUsed = 1
                         alert('The shuttle is arriving!')
-                
+                        clearInterval(etaCalculation) //stops the constant interval every 10 seconds so we prevent unneccessary fetches to the campus doublemap
                     }
 
-                    else if (eta <= timeToAlert && hastimeToAlertBeenUsed = 0) {
-                        console.log('The shuttle is arriving!')
-                        hastimeToAlertBeenUsed = 1
-                        alert('Time to leave! Your designated time has arrived at ' + eta + ' minute')
-                    }
-    
-                }*/
+                
+                }
             }, 10e3);
-            //var objectPosition = prompt("What is the object's position in the json");
-            document.getElementById('input').innerHTML = "Your stop ID is " + input;
-            //document.getElementById('objectPosition').innerHTML = objectPosition;
             };  
         </script>
     </html>`;
@@ -126,45 +154,45 @@ if (req.url === "/") {
     res.write(formSubmission);
     res.end();
 }
+
+// This was shamelessly stolen from stackoverflow here: https://stackoverflow.com/questions/12006417/node-js-server-that-accepts-post-requests
+else if (req.url.match("/post-eta")) {
+    if (req.method == 'POST') { // When the client submits the form, it posts data to /post-eta
+        console.log('POST RECEIVED')
+        req.on('data', function(data) { // This stored the data as the input variable to be used in calculating the ETA
+          input = data
+          console.log('The data received is ' + input)
+        })
+        req.on('end', async function() { // This sends the client back to the index.html of the file.
+          res.writeHead(302, {'Location': '/'})
+          res.end()
+        }) 
+    }
+}
 else if (req.url.match("/get-eta")) {
-    if (input === '') { //essentially if the input is still at the starting variable
+    if (input === '') { //essentially if the input is still at the starting variable, it indicates the client hasn't sent anything yet, so we prevent the scraper from being initialized, limiting unneccesary calls to the doublemap
         console.log('There has not been any input sent yet!')
         res.writeHead(200, { "Content-Type": "text/html" })
-        res.write('(awaiting)')
+        res.write('(awaiting)') // This shows the client that it is awaiting for the scraper to gather the ETA, or that the client hasn't submitted any form data yet.
         res.end()
     }
     else if (input !== '') {
-        let eta = await scraper(input)
+        let eta = await scraper(input) // When the input submitted by the form is anything other than the starting variable, the scraper can begin. Every time localhost:3000/get-eta is refreshed, the scraper sends another request to the doublemap to get the ETA.
         console.log(eta)
         res.write(eta.toString())
         res.end()
     }
-    
-    
-// res.write();
-//res.end(await scraper(input))
-//res.end(ETA.toString())
 }
 
-// This was shamelessly stolen from stackoverflow here: https://stackoverflow.com/questions/12006417/node-js-server-that-accepts-post-requests
-else if (req.url.match("/post-eta")) {
-    if (req.method == 'POST') {
-        console.log('POST RECEIVED')
-
-
-        req.on('data', function(data) {
-          input = data
-          console.log('The data received is ' + input)
-        })
-        req.on('end', async function() {
-          res.writeHead(302, {'Location': '/'})
-          res.end()
-
-        }) 
-    }
+else if (req.url.match("favicon.ico")) {
+    res.writeHead(200,{'Content-type':'image/ico'});
+    let img = fs.readFileSync('./project3/favicon.ico');
+    res.write(img, 'binary');
+    res.end()
 }
+
 else {
-    res.writeHead(404, { "Content-Type": "text/html" })
+    res.writeHead(404, { "Content-Type": "text/html" }) // If the client submits a url that is anything else other than the above directories, this catches the request and tells them it isn't found.
     res.end(`404 file not found at ${req.url}`);
 }
 }).listen(3000) 
@@ -172,10 +200,10 @@ console.log("Server listening on port 3000");
 
 //scraper(input)
 async function scraper(input) {
-    dataChunk = input.split("&") // This splits the two form elements that are received, the shuttle stop ID and the object position
+    let dataChunk = input.toString().split("&") // This splits the two form elements that are received, the shuttle stop ID and the object position
     //input[0] = input.split("=").pop()
-    stopID = dataChunk[0].split("=").pop() //Takes the form data and uses the first element that is send (which is the stopID), and removed the input field name and the equal sign, so I am only left with the integers
-    objectPosition = dataChunk[1].split("=").pop() //Takes the form data and uses the second element that is send (which is the objectPosition), and removed the input field name and the equal sign, so I am only left with the integers
+    let stopID = dataChunk[0].split("=").pop() //Takes the form data and uses the first element that is send (which is the stopID), and removed the input field name and the equal sign, so I am only left with the integers
+    let objectPosition = dataChunk[1].split("=").pop() //Takes the form data and uses the second element that is send (which is the objectPosition), and removed the input field name and the equal sign, so I am only left with the integers
 
     console.log('The current stopID is ' + stopID)
     console.log('The current objectPosition is ' + objectPosition)
@@ -184,69 +212,5 @@ async function scraper(input) {
     const URL = `https://uc.doublemap.com/map/v2/eta?stop=${stopID}`;
     console.log("Parsing the url: " + URL);
     const response = await axios.get(URL);
-    return response.data.etas[stopID].etas[objectPosition]['avg'];     //const URL = `https://uc.doublemap.com/map/v2/eta?stop=${input}`;
-    //console.log("Watching the url: " + URL);
-    //const response = await axios.get(URL);
-    //return response.data.etas[input].etas[objectPosition]['avg'];
+    return response.data.etas[stopID].etas[objectPosition]['avg']; 
   } 
-
-//getETA("=129")
-async function getETA(input) {
-    console.log(await testing(input))
-}
- async function testing(input) {
-    input = input.split("=").pop()
-    objectPosition = 0
-    const URL = `https://uc.doublemap.com/map/v2/eta?stop=${input}`;
-    console.log("Watching the url: " + URL);
-    const response = await axios.get(URL);
-    return 'Your ETA is ' + response.data.etas[input].etas[objectPosition]['avg']; 
- }
-
-function isItTimeToLeave(eta) {
-    console.log('The ETA for the shuttle is ' + eta + ' minute(s)')
-    //These are a series of if statements that are checking the ETA compared to the time you need to leave.
-    if (eta === 5) {
-        console.log('There is five minutes until the shuttle is arriving!')
-        alert('There is 5 minutes!')
-    }
-    if (eta === 1) {
-        console.log('There is one minute until the shuttle is arriving!')
-        alert('There is 1 minute!')
-
-    }
-    if (eta === timeToGetThere) {
-        console.log('Leave now to make it to the stop in time!')
-        alert('Leave now to make it to the stop in time!')
-
-    }
-    if (eta === 0) {
-        console.log('The shuttle is arriving!')
-        alert('The shuttle is arriving!')
-
-    }
-    if (eta !== 0) {
-        setTimeout(collectTheETA, 5000);
-    }
-}
-
-
-
-//function isTimeToLeaveConditionMet
-
-/* function whenIsItTimeToLeave() {
-    console.log(eta)
-} */
-
-/* async function shuttleScraper() {
-    const response =  axios.get(URL);
-    const eta = response.data.eta[input].etas[0]['avg'];
-    console.log(eta);
-    //return eta;
-} */
-
-/*
-function collectTheETA() {
-    const theETA =  shuttleScraper();
-    return theETA;
-} */
